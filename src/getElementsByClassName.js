2019822e2dc2f9b42791ef2a8ae2a54d367fc08f
MKS-students <mks.students@hackreactor.com>
2015-07-27 12:35:39 -0500

    working on problem

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -2,9 +2,26 @@
 // var getElementsByClassName = function (className) {
 //   return document.getElementsByClassName(className);
 // };
-
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
+var getElementsByClassName = function(className, node, output) {
   // your code here
+  //get the node
+  if (node === undefined){
+    var node = document.body;
+  }
+  if (output === undefined){
+    var output = [];
+  }
+  //test node for the given className
+  if (node.classList.contains(className)) {
+    output.push(node);
+  }
+  //if node has given class name then push on to new array
+  for (var i = 0; i < node.childNodes.length; i++) {
+    getElementsByClassName(className, node.childNodes[i], output);
+  }
+  return output;
+  //check the children of document.body for the given className
+  //if any of the children contain the className push it on to the new array
+  //return the newArray
 };
